17c46da473df328f56531f352bd9b230a1816de9
Aneesh1829 <aneeshreddy005@gmail.com>
2025-10-19 13:23:08 +0530

    Initial commit for fresh deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TaskMaker/'  // MUST match your repo name exactly
+  base: '/TaskMaker-Deploy/'   // MUST match new repo name exactly
 })
